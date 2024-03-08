78786e5c99a864242b3ef4bf8e160165d4a2a640
jl917 <julong1988@naver.com>
2024-03-08 20:54:54 +0900

    easy includes  보충

--- 202401/easy/00898-easy-includes.ts
+++ 202401/easy/00898-easy-includes.ts
@@ -1,3 +1,10 @@
+import { Equal } from "../../types";
 namespace namespace {
+  type Includes<T extends any[], K> = T extends [infer F, ...infer R]
+    ? Equal<F, K> extends true
+      ? true
+      : Includes<R, K>
+    : false;
+
   type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">; // expected to be `false`
 }
